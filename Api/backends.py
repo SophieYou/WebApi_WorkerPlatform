2e714e9a2633ac46fa1daedb39fe46125e57da5d
Sophie You <fjptynt@gmail.com>
2021-10-25 18:25:13 +0800

    version 1.1.0

--- Api/backends.py
+++ Api/backends.py
@@ -7,9 +7,11 @@ class CustomAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         user = User.objects.get(username=username)
         v_code = False
-        if 'data' in request:
+        try:
             if 'verify_code' in request.data:
                 v_code = request.data["verify_code"]
+        except:
+            pass
 
         print("verify code is " + str(v_code))
         print("user is " + user.username)
